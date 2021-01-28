9139d430f93a7fd421a1a8daa3197eec0e4caf29
Massinelo <massi.ait@gmail.com>
2021-01-28 05:02:39 +0100

    css V2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <span>made with</span>
-      <a href="https://reactjs.org/">React</a>
-      <span>@</span>
-      <a href="https://www.lereacteur.io/">Le Reacteur</a>
-      <span>by</span>
+    <footer>
+      <span>made with </span>
+      <a href="https://reactjs.org/">React </a>
+      <span>@ </span>
+      <a href="https://www.lereacteur.io/">Le Reacteur </a>
+      <span>by </span>
       <a href="https://github.com/Massinelo">Massinelo</a>
-    </div>
+    </footer>
   );
 };
 
 export default Footer;
+
+<footer>
+  Made with <a href="https://reactjs.org/">React</a> @{' '}
+  <a href="https://www.lereacteur.io/">Le Reacteur</a> by{' '}
+  <a href="https://github.com/jolisdegats">Jolisdegats</a>{' '}
+</footer>;
